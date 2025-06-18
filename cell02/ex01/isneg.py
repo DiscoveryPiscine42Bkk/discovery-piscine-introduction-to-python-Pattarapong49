5305859d4b5a1c038b6644dc8d68f7355f569859
Pattarapong49 <pattarapongjaonaay49@gmail.com>
2025-06-18 07:04:54 +0000

    add isneg.py

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,7 +1,7 @@
 value = int(input())
-if value > 0:
+if value > 1:
 	print("This number is positive.")
-elif value < 0:
+elif value < 1:
 	print("This number is negative.")
 else:
 	print("This number is both positive and negative.")
